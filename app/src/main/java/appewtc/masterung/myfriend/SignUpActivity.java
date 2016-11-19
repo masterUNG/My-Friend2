14df177fd1a0aad09e218ab2d909e58bf9340ca2
masterUNG <phrombutr@gmail.com>
2016-11-19 14:45:04 +0700

    Re Design

--- app/src/main/java/appewtc/masterung/myfriend/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/myfriend/SignUpActivity.java
@@ -1,7 +1,13 @@
 package appewtc.masterung.myfriend;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -58,12 +64,39 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
 
             }   // onClick
         });
 
-
     }   // Main Method
+
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if ((requestCode == 0) && (resultCode == RESULT_OK)) {
+
+            //Take Photo OK
+            Log.d("19novV1", "Take Photo OK");
+
+            //Setup Image Take to ImageView
+            Uri uri = data.getData();
+
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+                avataImageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                Log.d("19novV1", "e ==> " + e.toString());
+            }   // try
+
+        }   // if
+
+    } // onActivityResult
 
 }   // Main Class
