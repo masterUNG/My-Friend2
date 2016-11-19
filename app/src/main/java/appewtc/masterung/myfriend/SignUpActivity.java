9d02c2579acf0d88e4b5b8144f3a0d307e833238
masterUNG <phrombutr@gmail.com>
2016-11-19 13:48:16 +0700

    Take Photo

--- app/src/main/java/appewtc/masterung/myfriend/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/myfriend/SignUpActivity.java
@@ -2,13 +2,68 @@ package appewtc.masterung.myfriend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText, passwordEditText;
+    private ImageView avataImageView, takePhotoImageView;
+    private Button button;
+    private String nameString, userString, passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        avataImageView = (ImageView) findViewById(R.id.imageView);
+        takePhotoImageView = (ImageView) findViewById(R.id.imageView2);
+        button = (Button) findViewById(R.id.button3);
+
+
+        //Button Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Get Value From Edit Text
+                nameString = nameEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+                    //Have Space
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.doremon48,
+                            "Have Space", "Please Fill All Every Blank");
+                    myAlert.myDialog();
+
+                }   // if
+
+            }   // onClick
+        });
+
+
+        //TakePhoto Controller
+        takePhotoImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }   // onClick
+        });
+
+
     }   // Main Method
 
 }   // Main Class
