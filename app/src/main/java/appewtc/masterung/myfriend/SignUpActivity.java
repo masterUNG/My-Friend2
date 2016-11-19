626515d2da80362c26f21631c3a60f5c25d7ea77
masterUNG <phrombutr@gmail.com>
2016-11-19 10:32:51 +0700

    Intent OK1

--- app/src/main/java/appewtc/masterung/myfriend/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/myfriend/SignUpActivity.java
@@ -3,12 +3,11 @@ package appewtc.masterung.myfriend;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }   // Main Method
-
-}   // Main Class
+        setContentView(R.layout.activity_sign_up);
+    }
+}
