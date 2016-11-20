aa7b0ce2f3a7332baea7c7a2320b45fad6ad708c
masterUNG <phrombutr@gmail.com>
2016-11-20 14:05:04 +0700

    Main

--- app/src/main/java/appewtc/masterung/myfriend/MainActivity.java
+++ app/src/main/java/appewtc/masterung/myfriend/MainActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private Button signInButton, signUpButton;
+    private EditText userEditText, passwordEditText;
 
 
     @Override
