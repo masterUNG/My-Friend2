26c97a836b089407d45ba4a177a77c5a94a96062
masterUNG <phrombutr@gmail.com>
2016-11-20 14:21:09 +0700

    Check Space

--- app/src/main/java/appewtc/masterung/myfriend/MainActivity.java
+++ app/src/main/java/appewtc/masterung/myfriend/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     //Explicit
     private Button signInButton, signUpButton;
     private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
 
     @Override
@@ -22,6 +23,30 @@ public class MainActivity extends AppCompatActivity {
         //Bind Widget
         signInButton = (Button) findViewById(R.id.button);
         signUpButton = (Button) findViewById(R.id.button2);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+
+        //SignIn Controller
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Get Value
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if ((userString.length() == 0) || (passwordString.length() == 0)) {
+                    MyAlert myAlert = new MyAlert(MainActivity.this,
+                            R.drawable.kon48, "มีช่องว่าง", "กรอกทุกชอง คะ");
+                    myAlert.myDialog();
+                } else {
+                    checkUserPassword();
+                }
+
+            }   // onClick
+        });
+
 
         //SignUp Controller
         signUpButton.setOnClickListener(new View.OnClickListener() {
@@ -32,5 +57,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }   // Main Method
+
+    private void checkUserPassword() {
+
+    }
 
 }   // Main Class
